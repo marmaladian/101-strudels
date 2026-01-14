f8d53a67d1b615a11bd5aa0e661934cbaf7dde16
marmaladian <benjamin@gray.land>
2026-01-14 15:03:55 +1100

    002 cool shoes

---
+++ strudel-002.js
@@ -0,0 +1,21 @@
+// 002
+// cool shoes
+// 2026-01-14
+
+// tried to copy '24 hours - victor flex'
+
+setcpm(130/4)
+// rhodespolaris_bd(4)
+$: s("[bd - [- bd] -]").bank("rhodespolaris")._punchcard()
+$: s("- sd - sd").bank("compurhythm78")._punchcard()
+$: s("[- hh] [hh [- hh]] [oh hh oh hh] [oh oh hh -]").bank("compurhythm78").gain(0.5)._punchcard()
+// hh oh rim
+
+// questions
+// the base drum punchcard makes it look like the second
+//   kick will play for half as long, but it's just the trigger timing.
+//   is there a better way to display this?
+
+// next
+// add some pads
+// figure out how to make a few hat patterns and alternate between them
